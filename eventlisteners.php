5ac48dc44e4a5e061c2675d87597ff3d44287e9c
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-07-01 00:53:59 -0300

    fix eventlisteners

--- eventlisteners.php
+++ eventlisteners.php
@@ -4,37 +4,56 @@ namespace Log\EventListeners;
 
 use SplitPHP\Database\Dao;
 use SplitPHP\EventListener;
+use SplitPHP\Database\Dbmetadata;
+use Exception;
 
 class Listeners extends EventListener
 {
   public function init()
   {
     $this->addEventListener('log.any', function ($event) {
-      // Handle the log event
-      $this->getDao('LOG_RECORD')
-        ->insert([
-          'ds_key' => 'log-' . uniqid(),
-          'dt_log' => $event->getDatetime(),
-          'ds_context' => $event->getLogName(),
-          'tx_message' => json_encode($event->getLogMsg()) ?? $event->getLogMsg(),
-          'ds_filepath' => $event->getLogFilePath()
-        ]);
+      if (!Dbmetadata::tableExists('LOG_RECORD')) {
+        // If the table does not exist, we cannot log
+        return;
+      }
 
-      Dao::flush();
+      try {
+        // Handle the log event
+        $this->getDao('LOG_RECORD')
+          ->insert([
+            'ds_key' => 'log-' . uniqid(),
+            'dt_log' => $event->getDatetime(),
+            'ds_context' => $event->getLogName(),
+            'tx_message' => json_encode($event->getLogMsg()) ?? $event->getLogMsg(),
+            'ds_filepath' => $event->getLogFilePath()
+          ]);
+
+        Dao::flush();
+      } catch (Exception $e) {
+      }
     });
 
     $this->addEventListener('log.error', function ($event) {
-      // Handle the log event
-      $this->getDao('LOG_RECORD')
-        ->insert([
-          'ds_key' => 'log-' . uniqid(),
-          'dt_log' => $event->getDatetime(),
-          'ds_context' => $event->getLogName(),
-          'ds_filepath' => $event->getLogFilePath(),
-          'tx_message' => json_encode($event->getLogMsg()) ?? $event->getLogMsg(),
-        ]);
+      if (!Dbmetadata::tableExists('LOG_RECORD')) {
+        // If the table does not exist, we cannot log
+        return;
+      }
 
-      Dao::flush();
+      try {
+        // Handle the log event
+        $this->getDao('LOG_RECORD')
+          ->insert([
+            'ds_key' => 'log-' . uniqid(),
+            'dt_log' => $event->getDatetime(),
+            'ds_context' => $event->getLogName(),
+            'tx_message' => json_encode($event->getLogMsg()) ?? $event->getLogMsg(),
+            'ds_filepath' => $event->getLogFilePath()
+          ]);
+
+        Dao::flush();
+      } catch (Exception $e) {
+        // Handle any exceptions that may occur during logging
+      }
     });
   }
 }
